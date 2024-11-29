f6400eee8a4c7f0918d9a37b5fd23035104d99ef
ksj42 <zerojin0212@gmail.com>
2024-11-29 20:29:41 +0900

    Automated commit on 2024-11-29 20:29:41.35

--- app/src/main/java/com/example/myapplication/ActivityTwoplayerview.kt
+++ app/src/main/java/com/example/myapplication/ActivityTwoplayerview.kt
@@ -1,9 +1,15 @@
 package com.example.myapplication
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivityTwoplayerview : AppCompatActivity() {
+
+    // mediaPlayer 객체 선언
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.two_player_view)  // 새로운 레이아웃 설정
@@ -11,6 +17,20 @@ class ActivityTwoplayerview : AppCompatActivity() {
         supportActionBar?.hide()
         window.statusBarColor = resources.getColor(android.R.color.black, null)
 
+        // MediaPlayer 초기화
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound_stone5)
+    }
 
+    fun onGoClick(view: View) {
+        val boardView = findViewById<CustomBoardView2>(R.id.boardView) // 코드 들고오기
+        boardView.placeStone()
+        mediaPlayer.start() // 효과음 재생
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Activity 종료 시 mediaPlayer 해제
+        mediaPlayer.release()
     }
 }
+
